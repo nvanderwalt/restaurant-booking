9da23797c934a85cce5b29d9ef068965a3dd90dc
nvanderwalt <nvanderwalt12@yahoo.com>
2025-03-31 15:47:13 +0200

    #added date & time input to forms.py | added *.sqlite3 to gitignore

--- booking/forms.py
+++ booking/forms.py
@@ -4,3 +4,8 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Booking, Table, MenuItem
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+class TimeInput(forms.TimeInput):
+    input_type = 'time'
