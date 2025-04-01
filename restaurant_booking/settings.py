30dbdd6801175538b397aaff0e01a88483923a45
nvanderwalt <nvanderwalt12@yahoo.com>
2025-04-01 08:44:38 +0200

    #fixed templates dir in settings.py (page viewable)

--- restaurant_booking/settings.py
+++ restaurant_booking/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'restaurant_booking.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
