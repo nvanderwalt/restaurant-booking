fad039d387780183c61c9d5cce8042356ba0a17d
nvanderwalt <nvanderwalt12@yahoo.com>
2025-04-01 12:35:12 +0200

    #Fixed UserCreate View

--- booking/views.py
+++ booking/views.py
@@ -1,6 +1,7 @@
 
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.http import JsonResponse
 from django.contrib.admin.views.decorators import staff_member_required
