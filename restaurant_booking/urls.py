3e3d4d19af05ec8a7c248723eec317156ac18c33
nvanderwalt <nvanderwalt12@yahoo.com>
2025-04-03 14:35:25 +0200

    #Fixed logout function

--- restaurant_booking/urls.py
+++ restaurant_booking/urls.py
@@ -3,10 +3,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from booking import views as booking_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('booking.urls')),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('register/', booking_views.register, name='register'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
