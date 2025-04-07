15ffd6b1c248f36ce6971f0123e21ed5772d7f56
nvanderwalt <nvanderwalt12@yahoo.com>
2025-04-07 17:21:46 +0200

    #added email 'required' when signing up & test if email exist

--- booking/forms.py
+++ booking/forms.py
@@ -37,13 +37,19 @@ class BookingForm(forms.ModelForm):
     
     
 class RegisterForm(UserCreationForm):
-    email = forms.EmailField()
+    email = forms.EmailField(required=True)
     first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
     last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
     
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("This email address is already in use.")
+        return email
 
 class TableForm(forms.ModelForm):
     class Meta:
